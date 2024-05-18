788238e5df712394b4ee4b6af68feaae9cead71e
Shravya-Rao-B <shravyar8@gmail.com>
2024-05-18 22:13:47 +0800

    react multiple components

---
+++ LLD_Frontend/module3/react/scaler-assignments/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+// import Gallery from "./Gallery";
+import List from "./List";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Gallery /> */}
+      <List />
+    </div>
+  );
+}
+
+export default App;
