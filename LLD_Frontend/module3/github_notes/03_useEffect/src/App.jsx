e6dd3d56288d49a112ff197ad32f22c96fbdfcf6
Shravya-Rao-B <shravyar8@gmail.com>
2024-07-15 16:10:00 +0800

    all uncommitted code

---
+++ LLD_Frontend/module3/github_notes/03_useEffect/src/App.jsx
@@ -0,0 +1,12 @@
+// import HandleAllTodo from './components/todoList/HandleAllTodo';
+// import GetData from './components/useEffectExamples/GetData';
+import UseEffectExamples from './components/useEffectExamples/UseEffectExamples';
+function App() {
+return <div>
+  {/* <HandleAllTodo/> */}
+  {/* // <GetData/> */}
+  <UseEffectExamples />
+</div>
+}
+
+export default App
